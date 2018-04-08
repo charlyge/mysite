5f1494f45fa66fce257cd871cfd6f42db879404d
charlyge <charlyge2@gmail.com>
2018-04-08 21:31:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<Html>
+<title>
+HNG
+</title>
+<head>
+<link rel="Stylesheet" type="text/css" href="Style.css">
+</head>
+
+<body>
+ <h1 id="firstH">My first Figma Design</h1>
+  <h3 id="secondH">Time is 6:14:40 Pm</h3>
+   <h2>Saturday April,7,2018</h2>
+   
+
+</body>
+
+</Html>
